0598ffd34e51fb226a7478739e61498f8d0fd0c4
LSH <k01094060462@gmail.com>
2024-07-22 13:46:25 +0900

    base

---
+++ src/main/java/com/sparta/nexusteam/base/CommonResponse.java
@@ -0,0 +1,12 @@
+package com.sparta.nexusteam.base;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CommonResponse {
+    private int statusCode;
+    private String message;
+    private Object data;
+}
---
+++ src/main/java/com/sparta/nexusteam/base/ControllerUtil.java
@@ -0,0 +1,45 @@
+package com.sparta.nexusteam.base;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Slf4j(topic = "Controller")
+public final class ControllerUtil {
+
+    private ControllerUtil() {
+    }
+
+    public static ResponseEntity<CommonResponse> getFieldErrorResponseEntity(BindingResult bindingResult, String message) {
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        for (FieldError fieldError : fieldErrors) {
+            log.error("{} field : {}", fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return org.springframework.http.ResponseEntity.badRequest()
+                .body(CommonResponse.<List<FieldError>>builder()
+                        .statusCode(HttpStatus.BAD_REQUEST.value())
+                        .message(message)
+                        .data(fieldErrors)
+                        .build());
+    }
+
+    public static ResponseEntity<CommonResponse> getBadRequestResponseEntity(Exception e) {
+        return org.springframework.http.ResponseEntity.badRequest().body(CommonResponse.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .message(e.getMessage())
+                .build());
+    }
+
+    public static ResponseEntity<CommonResponse> getResponseEntity(Object response, String message) {
+        return org.springframework.http.ResponseEntity.ok().body(CommonResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message(message)
+                .data(response)
+                .build());
+    }
+}
