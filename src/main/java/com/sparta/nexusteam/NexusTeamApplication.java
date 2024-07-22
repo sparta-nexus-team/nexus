91d8a30ad72b36c2e3ae7a0e7887a39bc067aaba
LSH <k01094060462@gmail.com>
2024-07-22 13:43:40 +0900

    init

---
+++ src/main/java/com/sparta/nexusteam/NexusTeamApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nexusteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/nexusteam/NexusTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nexusteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
