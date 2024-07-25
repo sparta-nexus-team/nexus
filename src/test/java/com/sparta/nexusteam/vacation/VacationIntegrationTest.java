b6f8b6f63e7df061db1376af0b64ca3d8c5a0a18
jaewonchoi1234 <qwaszx9743@naver.com>
2024-07-25 12:21:14 +0900

    'feat' : 휴가 테스트 코드 추가(파일만 생성)

---
+++ src/test/java/com/sparta/nexusteam/vacation/VacationIntegrationTest.java
@@ -0,0 +1,8 @@
+package com.sparta.nexusteam.vacation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class VacationIntegrationTest {
+
+}
---
+++ src/test/java/com/sparta/nexusteam/vacation/VacationServiceTest.java
@@ -0,0 +1,5 @@
+package com.sparta.nexusteam.vacation;
+
+public class VacationServiceTest {
+
+}
